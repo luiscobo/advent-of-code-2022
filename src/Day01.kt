c24769aeeb2e9efa2e51e8cbd20bc101946d6457
Luis Cobo <luis.cobo@gmail.com>
2022-12-04 21:24:49 -0500

    Commit de la solución del problema del primer día.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,45 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var n = 0
+        var mayor = 0
+        var suma = 0
+
+        input.forEach {
+            if (it.trim().isEmpty()) {
+                n++
+                if (suma > mayor) {
+                    mayor = suma
+                }
+                suma = 0
+            }
+            else {
+                suma += it.toInt()
+            }
+        }
+        n++
+        if (suma > mayor) {
+            mayor = suma
+        }
+        return mayor
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val caloriasElfos = mutableListOf<Int>()
+        var suma = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        input.forEach {
+            if (it.trim().isEmpty()) {
+                caloriasElfos.add(suma)
+                suma = 0
+            }
+            else {
+                suma += it.toInt()
+            }
+        }
+        caloriasElfos.add(suma)
+        caloriasElfos.sortDescending()
+        return caloriasElfos.take(3).sum()
+    }
 
     val input = readInput("Day01")
     println(part1(input))
