3f8bc79570a96ab0e5324c09c0f596b944658f06
Luis Cobo <luis.cobo@gmail.com>
2022-12-09 14:05:08 -0500

    Del Día 06

---
+++ src/Day06.kt
@@ -0,0 +1,32 @@
+fun main() {
+    fun todosDiferentes(buffer: String, n: Int, cuantos: Int): Boolean {
+        if (n < buffer.length) {
+            val seccion = buffer.substring(n - cuantos + 1 .. n)
+            return seccion.toCharArray().distinct().size == cuantos
+        }
+        return false
+    }
+
+
+    fun part1(input: String): Int {
+        for (i in 3 until input.length) {
+            if (todosDiferentes(input, i, 4)) {
+                return i + 1
+            }
+        }
+        return -1
+    }
+
+    fun part2(input: String): Int {
+        for (i in 13 until input.length) {
+            if (todosDiferentes(input, i, 14)) {
+                return i + 1
+            }
+        }
+        return -1
+    }
+
+    val input = readInput("Day06")
+    println(part1(input[0]))
+    println(part2(input[0]))
+}
